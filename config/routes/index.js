929e5214b80efcb84c4b5b612c066a9c3cbedacb
Albert Abril <albert.abril@gmail.com>
2017-02-14 13:47:57 +0000

    new layout added with browsersync and tachyons

--- config/routes/index.js
+++ config/routes/index.js
@@ -3,4 +3,5 @@ module.exports = function(app){
     require('./site_api_samples')(app);
     require('./admin')(app);
     require('./custom')(app);
+    require('./tachyons')(app);
 }
--- config/routes/tachyons.js
+++ config/routes/tachyons.js
@@ -1,5 +1,5 @@
 const passportConfig = require('../passport');
-const homeController = require('../../controllers/custom/home');
+const homeController = require('../../controllers/tachyons/home');
 const authService = require('../authService');
 const lessExpress = require('less-express')
 
@@ -7,6 +7,6 @@ const path = require('path');
 const customMainLessPath = path.join(__dirname, '../../public/custom/styles/main.less');
 
 module.exports = function(app){
-    app.get('/custom', homeController.index);
+    app.get('/', homeController.index);
     app.get('/custom/styles/main.min.css', lessExpress(customMainLessPath));
 }
